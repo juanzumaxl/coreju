a8983bc28f0f10b1cf0ed16fb358cc3955d29a11
juanzumaxl <145026381+juanzumaxl@users.noreply.github.com>
2023-11-13 01:37:58 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,53 +1,56 @@
-﻿const express = require('express');
+const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 const bodyParser = require('body-parser');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// Conectar a la base de datos MongoDB
-mongoose.connect('mongodb+srv://Coreju:Coreju2023@corejucluster.wjl4yiy.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
-
-// Definir el esquema del modelo
-const participantSchema = new mongoose.Schema({
-    fullName: String,
-    location: String,
-    roomNumber: String,
-    phoneNumber: String,
-    email: String
+// Configuración de MongoDB
+mongoose.connect('mongodb+srv://Coreju:Coreju2023@corejucluster.wjl4yiy.mongodb.net/?retryWrites=true&w=majority', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
 });
 
-// Crear el modelo Participant
+// Definición del esquema de MongoDB
+const participantSchema = new mongoose.Schema({
+  fullName: String,
+  location: String,
+  roomNumber: String,
+  phoneNumber: String,
+  email: String
+});
+
 const Participant = mongoose.model('Participant', participantSchema);
 
-// Configurar bodyParser para analizar el cuerpo de las solicitudes
-app.use(bodyParser.urlencoded({ extended: true }));
+// Configuración de Express
+app.use(cors());
 app.use(bodyParser.json());
+app.use(express.static('public'));
 
-// Endpoint para obtener todos los participantes registrados
-app.get('/api/participants', async (req, res) => {
-    try {
-        const participants = await Participant.find();
-        res.json(participants);
-    } catch (error) {
-        res.status(500).json({ error: 'Error al obtener participantes' });
-    }
+// Rutas
+app.post('/api/participants', async (req, res) => {
+  try {
+    const newParticipant = new Participant(req.body);
+    await newParticipant.save();
+    res.json({ message: 'Participante registrado exitosamente.' });
+  } catch (error) {
+    console.error('Error al registrar participante:', error);
+    res.status(500).json({ message: 'Error al registrar participante.' });
+  }
 });
 
-// Endpoint para registrar un nuevo participante
-app.post('/api/participants', async (req, res) => {
-    const { fullName, location, roomNumber, phoneNumber, email } = req.body;
-
-    try {
-        const newParticipant = new Participant({ fullName, location, roomNumber, phoneNumber, email });
-        await newParticipant.save();
-        res.json({ message: 'Participante registrado exitosamente' });
-    } catch (error) {
-        res.status(500).json({ error: 'Error al registrar participante' });
-    }
+app.get('/api/participants', async (req, res) => {
+  try {
+    const participants = await Participant.find();
+    res.json(participants);
+  } catch (error) {
+    console.error('Error al obtener participantes:', error);
+    res.status(500).json({ message: 'Error al obtener participantes.' });
+  }
 });
 
 // Iniciar el servidor
 app.listen(PORT, () => {
-    console.log(`Servidor escuchando en el puerto ${PORT}`);
+  console.log(`Servidor en ejecución en http://localhost:${PORT}`);
 });
