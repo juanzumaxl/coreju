29912bdc25e39509eb37110a86c40d98bd721f7d
juanzumaxl <145026381+juanzumaxl@users.noreply.github.com>
2023-11-13 01:08:07 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,65 @@
+﻿// Cambia la URL del servidor a la que estés utilizando
+const API_URL = ''mongodb+srv://Coreju:Coreju2023@corejucluster.wjl4yiy.mongodb.net/?retryWrites=true&w=majority'';
+
+function submitRegistration() {
+    var fullName = document.getElementById("fullName").value;
+    var location = document.getElementById("location").value;
+    var roomNumber = document.getElementById("roomNumber").value;
+    var phoneNumber = document.getElementById("phoneNumber").value;
+    var email = document.getElementById("email").value;
+
+    var registrationData = {
+        fullName: fullName,
+        location: location,
+        roomNumber: roomNumber,
+        phoneNumber: phoneNumber,
+        email: email
+    };
+
+    // Realizar una solicitud POST al servidor para registrar el participante
+    fetch(API_URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(registrationData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            alert(data.message);
+            loadRegisteredParticipants(); // Actualizar la lista después de registrar
+        })
+        .catch(error => {
+            console.error('Error al registrar participante:', error);
+            alert('Error al registrar participante. Consulta la consola para más detalles.');
+        });
+
+    // Limpiar los campos del formulario después de registrar.
+    document.getElementById("registrationForm").reset();
+}
+
+function loadRegisteredParticipants() {
+    // Realizar una solicitud GET al servidor para obtener la lista de participantes
+    fetch(API_URL)
+        .then(response => response.json())
+        .then(participants => {
+            var participantsTable = document.getElementById("registeredParticipants");
+            participantsTable.innerHTML = ''; // Limpiar la tabla antes de volver a cargar
+
+            participants.forEach(function (participant) {
+                var row = participantsTable.insertRow();
+                row.insertCell(0).innerText = participant.fullName;
+                row.insertCell(1).innerText = participant.location;
+                row.insertCell(2).innerText = participant.roomNumber;
+                row.insertCell(3).innerText = participant.phoneNumber;
+                row.insertCell(4).innerText = participant.email;
+            });
+        })
+        .catch(error => {
+            console.error('Error al obtener participantes:', error);
+            alert('Error al obtener participantes. Consulta la consola para más detalles.');
+        });
+}
+
+// Llama a la función para cargar los participantes cuando la página se carga.
+window.onload = loadRegisteredParticipants;
---
+++ server.js
@@ -0,0 +1,53 @@
+﻿const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Conectar a la base de datos MongoDB
+mongoose.connect('mongodb+srv://Coreju:Coreju2023@corejucluster.wjl4yiy.mongodb.net/?retryWrites=true&w=majority', { useNewUrlParser: true, useUnifiedTopology: true });
+
+// Definir el esquema del modelo
+const participantSchema = new mongoose.Schema({
+    fullName: String,
+    location: String,
+    roomNumber: String,
+    phoneNumber: String,
+    email: String
+});
+
+// Crear el modelo Participant
+const Participant = mongoose.model('Participant', participantSchema);
+
+// Configurar bodyParser para analizar el cuerpo de las solicitudes
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Endpoint para obtener todos los participantes registrados
+app.get('/api/participants', async (req, res) => {
+    try {
+        const participants = await Participant.find();
+        res.json(participants);
+    } catch (error) {
+        res.status(500).json({ error: 'Error al obtener participantes' });
+    }
+});
+
+// Endpoint para registrar un nuevo participante
+app.post('/api/participants', async (req, res) => {
+    const { fullName, location, roomNumber, phoneNumber, email } = req.body;
+
+    try {
+        const newParticipant = new Participant({ fullName, location, roomNumber, phoneNumber, email });
+        await newParticipant.save();
+        res.json({ message: 'Participante registrado exitosamente' });
+    } catch (error) {
+        res.status(500).json({ error: 'Error al registrar participante' });
+    }
+});
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Servidor escuchando en el puerto ${PORT}`);
+});
